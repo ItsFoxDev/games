73e642789dbc8240082c8099eac8ff3d023fe797
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-01-13 19:52:39 -0500

    🎉 Unified setup JS

---
+++ setup.js
@@ -0,0 +1,17 @@
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
+document.title = gameid + ' | FoxDev';
+var iframe = document.createElement('iframe');
+iframe.src = srcurl;
+document.body.appendChild('iframe');
+changeFavicon(icourl);
